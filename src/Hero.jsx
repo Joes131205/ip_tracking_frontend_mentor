dd895713d583030f3324ef3eac15d701ab13b485
Joe Steven <stevenjoe1312@gmail.com>
2024-01-27 20:19:42 +0700

    initial

---
+++ src/Hero.jsx
@@ -0,0 +1,9 @@
+function Hero() {
+    return (
+        <div className="bg-[url('./assets/pattern-bg-mobile.png')] md:bg-[url('./assets/pattern-bg-desktop.png')] bg-cover w-screen h-56 text-center flex flex-col align-cente justify-center">
+            <h1 className="text-white text-5xl">IP Address Tracker</h1>
+        </div>
+    );
+}
+
+export default Hero;
