a6e9eb6da5c8e320526606840142d31699abb085
Joe Steven <stevenjoe1312@gmail.com>
2024-01-28 16:32:13 +0700

    handling form

--- src/Hero.jsx
+++ src/Hero.jsx
@@ -1,7 +1,39 @@
+import { useState } from "react";
+import arrow from "./assets/icon-arrow.svg";
+
 function Hero() {
+    const [data, setData] = useState("");
+    function handleChange(e) {
+        const ip = e.target;
+        setData(ip.value);
+        console.log(data);
+    }
+    function handleSubmit(e) {
+        e.preventDefault();
+        console.log(data + "? stolen heheha");
+    }
     return (
-        <div className="bg-[url('./assets/pattern-bg-mobile.png')] md:bg-[url('./assets/pattern-bg-desktop.png')] bg-cover w-screen h-56 text-center flex flex-col align-cente justify-center">
-            <h1 className="text-white text-5xl">IP Address Tracker</h1>
+        <div className="bg-[url('./assets/pattern-bg-mobile.png')] md:bg-[url('./assets/pattern-bg-desktop.png')] bg-cover w-screen h-72 text-center flex flex-col align-center justify-center gap-10">
+            <h1 className="text-white text-4xl">IP Address Tracker</h1>
+            <form
+                className="flex flex-row align-center justify-center"
+                onSubmit={handleSubmit}
+            >
+                <input
+                    type="text"
+                    placeholder="Search for any IP address or domain"
+                    className="w-[25rem] px-5 h-10 rounded-s-md"
+                    name="ip"
+                    onChange={handleChange}
+                    value={data}
+                />
+                <button
+                    type="submit"
+                    className="bg-black h-10 w-10 rounded-e-md flex justify-center items-center "
+                >
+                    <img src={arrow} className="w-2" />
+                </button>
+            </form>
         </div>
     );
 }
--- src/Info.jsx
+++ src/Info.jsx
@@ -1,7 +1,25 @@
 function Info() {
     return (
-        <div>
-            <h1></h1>
+        <div className="absolute top-[25%] left-[24%] bg-white px-10 py-10 rounded-lg">
+            <div className="grid grid-cols-4 divide-x w-[60rem] bg-[hsl(0, 0%, 59%)] gap-2 ">
+                <div className="px-10 ">
+                    <p className="text-sm">IP ADDRESS</p>
+                    <p className="font-bold text-2xl">192.212.174.101</p>
+                </div>
+                <div className="px-10">
+                    <p className="text-sm">LOCATION</p>
+                    <p className="font-bold text-2xl">Brooklyn, NY 10001</p>
+                </div>
+                <div className="px-10">
+                    <p className="text-sm">TIMEZONE</p>
+                    <p className="font-bold text-2xl">UTC -05:00</p>
+                </div>
+
+                <div className="px-10">
+                    <p className="text-sm">ISP</p>
+                    <p className="font-bold text-2xl">SpaceX Starlink</p>
+                </div>
+            </div>
         </div>
     );
 }
